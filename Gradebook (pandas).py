5a8e1cb5946ce17c4ec928b447096f3b46bcfe84
chloemaejohnson <64930495+chloemaejohnson@users.noreply.github.com>
2020-06-06 12:38:37 -0700

    Update Gradebook (pandas).py

--- Gradebook (pandas).py
+++ Gradebook (pandas).py
@@ -1,79 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import pandas as pd
 gradebook = pd.DataFrame({"Name":["Chloe", "Kenny", "Libby", "Oliver", "Lit"], 
                    "Grade":[95.5, 90.5, 80.0, 30.0, 75.0]}) 
 gradebook
 
-
-# In[2]:
-
-
 print("The average grade is ", gradebook["Grade"].mean(), ".")
 print("The highest score is ", gradebook["Grade"].max(), ".")
 print("The lowest score is ", gradebook["Grade"].min(), ".")
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[29]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[31]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
