3fa4f1ae4bd3511a6a8581532097a28a73a62e15
chloemaejohnson <64930495+chloemaejohnson@users.noreply.github.com>
2020-06-07 11:32:22 -0700

    Update Gradebook2.py

--- Gradebook2.py
+++ Gradebook2.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-grades = [10.0,24.5,17.9,54.8,75.8,99.0]
+grades = [100,87,93,45,38]
 x = len(grades)
 add = sum(grades)
 mean = add/x
-print("The average score of the grades is ", mean, ".")
-print("The highest score is ", max(grades), ".")
-print("The lowest score is ", min(grades), ".")
+
+def addScore(x):
+  grades.append(x)
+for i in grades:
+    if i > 100: print("Uh oh...Grade too large.")
+    if i < 1: print("Uh oh...Grade too small.")
+
+def calcStats():
+    return(mean(grades), min(grades), max(grades))
+print(calcStats)
+
+def printStats():
+   return("The average grade is " + mean + ". The highest grade is " + max + ". The lowest grade is " + min + "." ) 
+print(printStats)
